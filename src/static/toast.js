c1edefae8dca339709d0808b64126b937c0157ab
Tatva Agarwal <tatva.agarwal.2006@gmail.com>
2023-12-10 20:31:19 +0530

    Added dev name

--- src/static/toast.js
+++ src/static/toast.js
@@ -1,12 +1,12 @@
 function showtoast() {
-var toast = document.getElementById("toast");
-if (toast.innerHTML == ""){
-    console.log("no toast")
-    return
+    var toast = document.getElementById("toast");
+    if (toast.innerHTML == "") {
+        console.log("no toast")
+        return
 
-}
-toast.className = "show";
-  
-    
-setTimeout(()=>{ toast.className = toast.className.replace("show", ""); }, 5000);
+    }
+    toast.className = "show";
+
+
+    setTimeout(() => { toast.className = toast.className.replace("show", ""); }, 5000);
 }
